d5678d89b14441b37224e3ee0db3e511e12be8ea
Kat Schelonka <kschelonka@mozilla.com>
2024-01-04 16:03:52 -0800

    feat(router): infrastructure and ci/cd

---
+++ infrastructure/pocket-router/src/main.ts
@@ -0,0 +1,301 @@
+import { config } from './config';
+
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
+import { CloudwatchLogGroup } from '@cdktf/provider-aws/lib/cloudwatch-log-group';
+import { DataAwsCallerIdentity } from '@cdktf/provider-aws/lib/data-aws-caller-identity';
+import { DataAwsKmsAlias } from '@cdktf/provider-aws/lib/data-aws-kms-alias';
+import { DataAwsRegion } from '@cdktf/provider-aws/lib/data-aws-region';
+import { DataAwsSnsTopic } from '@cdktf/provider-aws/lib/data-aws-sns-topic';
+import { LocalProvider } from '@cdktf/provider-local/lib/provider';
+import { NullProvider } from '@cdktf/provider-null/lib/provider';
+import { PagerdutyProvider } from '@cdktf/provider-pagerduty/lib/provider';
+import { DataPagerdutyEscalationPolicy } from '@cdktf/provider-pagerduty/lib/data-pagerduty-escalation-policy';
+import {
+  PocketALBApplication,
+  PocketPagerDuty,
+  PocketAwsSyntheticChecks,
+} from '@pocket-tools/terraform-modules';
+
+import { App, S3Backend, TerraformStack } from 'cdktf';
+import { Construct } from 'constructs';
+import * as fs from 'fs';
+
+class PocketRoute extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, 'aws', { region: 'us-east-1' });
+    new LocalProvider(this, 'local_provider');
+    new NullProvider(this, 'null_provider');
+    new PagerdutyProvider(this, 'pagerduty_provider', { token: undefined });
+
+    const caller = new DataAwsCallerIdentity(this, 'caller');
+    const region = new DataAwsRegion(this, 'region');
+
+    new S3Backend(this, {
+      bucket: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
+      key: 'pocket-router',
+      region: region.name,
+    });
+
+    const pocketRouterPagerduty = this.createPagerDuty();
+    this.createPocketAlbApplication({
+      pagerDuty: pocketRouterPagerduty,
+      secretsManagerKmsAlias: this.getSecretsManagerKmsAlias(),
+      snsTopic: this.getCodeDeploySnsTopic(),
+      region,
+      caller,
+    });
+
+    new PocketAwsSyntheticChecks(this, 'synthetics', {
+      // alarmTopicArn:
+      //   config.environment === 'Prod'
+      //     ? pocketRouterPagerduty.snsCriticalAlarmTopic.arn // Tier 1
+      //     : '',
+      environment: config.environment,
+      prefix: config.prefix,
+      query: [],
+      shortName: config.shortName,
+      tags: config.tags,
+      uptime: [
+        {
+          response: 'ok',
+          // TODO: Do I need the port?
+          url: `${config.domain}:4000/.well-known/apollo/server-health`,
+        },
+      ],
+    });
+  }
+
+  /**
+   * Get the sns topic for code deploy
+   * @private
+   */
+  private getCodeDeploySnsTopic() {
+    return new DataAwsSnsTopic(this, 'backend_notifications', {
+      name: `Backend-${config.environment}-ChatBot`,
+    });
+  }
+
+  /**
+   * Get secrets manager kms alias
+   * @private
+   */
+  private getSecretsManagerKmsAlias() {
+    return new DataAwsKmsAlias(this, 'kms_alias', {
+      name: 'alias/aws/secretsmanager',
+    });
+  }
+
+  /**
+   * Create PagerDuty service for alerts
+   * @private
+   */
+  private createPagerDuty(): PocketPagerDuty | undefined {
+    if (config.isDev) {
+      //Dont create pagerduty services for a dev service.
+      return null;
+    }
+
+    const mozillaEscalation = new DataPagerdutyEscalationPolicy(
+      this,
+      'mozilla_sre_escalation_policy',
+      {
+        name: 'IT SRE: Escalation Policy',
+      },
+    );
+
+    return new PocketPagerDuty(this, 'pagerduty', {
+      prefix: config.prefix,
+      service: {
+        criticalEscalationPolicyId: mozillaEscalation.id,
+        nonCriticalEscalationPolicyId: mozillaEscalation.id,
+      },
+    });
+  }
+
+  private createPocketAlbApplication(dependencies: {
+    pagerDuty?: PocketPagerDuty;
+    region: DataAwsRegion;
+    caller: DataAwsCallerIdentity;
+    secretsManagerKmsAlias: DataAwsKmsAlias;
+    snsTopic: DataAwsSnsTopic;
+  }): PocketALBApplication {
+    const { pagerDuty, region, caller, secretsManagerKmsAlias, snsTopic } =
+      dependencies;
+
+    return new PocketALBApplication(this, 'application', {
+      internal: false,
+      prefix: config.prefix,
+      alb6CharacterPrefix: config.shortName,
+      tags: config.tags,
+      cdn: true,
+      domain: config.domain,
+      taskSize: {
+        cpu: 1024,
+        memory: 2048,
+      },
+      accessLogs: {
+        existingBucket: config.s3LogsBucket,
+      },
+      containerConfigs: [
+        {
+          name: 'app',
+          portMappings: [
+            {
+              hostPort: 4000,
+              containerPort: 4000,
+              protocol: 'tcp',
+            },
+          ],
+          envVars: [
+            {
+              name: 'PORT',
+              value: '4000',
+            },
+            {
+              name: 'APOLLO_GRAPH_REF',
+              value: `${config.envVars.graph.graphId}@${config.envVars.graph.graphVariant}`,
+            },
+            {
+              name: 'RELEASE_SHA',
+              value:
+                process.env.CODEBUILD_RESOLVED_SOURCE_VERSION ??
+                process.env.CIRCLE_SHA1,
+            },
+          ],
+          logGroup: this.createCustomLogGroup('app'),
+          logMultilinePattern: '^\\S.+',
+          secretEnvVars: [
+            {
+              name: 'APOLLO_KEY',
+              valueFrom: `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/APOLLO_KEY`,
+            },
+          ],
+          healthCheck: {
+            command: [
+              'CMD-SHELL',
+              'curl -f http://127.0.0.1:4000/.well-known/apollo/server-health || exit 1',
+            ],
+            interval: 15,
+            retries: 3,
+            timeout: 5,
+            startPeriod: 0,
+          },
+        },
+      ],
+      codeDeploy: {
+        useCodeDeploy: true,
+        useCodePipeline: false,
+        useTerraformBasedCodeDeploy: false,
+        snsNotificationTopicArn: snsTopic.arn,
+        notifications: {
+          //only notify on failed deploys
+          notifyOnFailed: true,
+          notifyOnStarted: false,
+          notifyOnSucceeded: false,
+        },
+      },
+      exposedContainer: {
+        name: 'app',
+        port: 4000,
+        healthCheckPath: '/.well-known/apollo/server-health',
+      },
+      ecsIamConfig: {
+        prefix: config.prefix,
+        taskExecutionRolePolicyStatements: [
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['secretsmanager:GetSecretValue', 'kms:Decrypt'],
+            resources: [
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared`,
+              `arn:aws:secretsmanager:${region.name}:${caller.accountId}:secret:Shared/*`,
+              secretsManagerKmsAlias.targetKeyArn,
+            ],
+            effect: 'Allow',
+          },
+          //This policy could probably go in the shared module in the future.
+          {
+            actions: ['ssm:GetParameter*'],
+            resources: [
+              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}`,
+              `arn:aws:ssm:${region.name}:${caller.accountId}:parameter/${config.name}/${config.environment}/*`,
+            ],
+            effect: 'Allow',
+          },
+        ],
+        taskRolePolicyStatements: [
+          {
+            actions: [
+              'xray:PutTraceSegments',
+              'xray:PutTelemetryRecords',
+              'xray:GetSamplingRules',
+              'xray:GetSamplingTargets',
+              'xray:GetSamplingStatisticSummaries',
+            ],
+            resources: ['*'],
+            effect: 'Allow',
+          },
+        ],
+        taskExecutionDefaultAttachmentArn:
+          'arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy',
+      },
+      autoscalingConfig: {
+        targetMinCapacity: config.isProd ? 4 : 2,
+        targetMaxCapacity: config.isProd ? 20 : 10,
+      },
+      alarms: {
+        http5xxErrorPercentage: {
+          //Triggers critical alert if 50% of request throws 5xx for
+          // 4 continuous evaluation period for 20 mins (5 mins per period)
+          threshold: 50,
+          evaluationPeriods: 4,
+          period: 300, //in seconds, 5 mins per period
+          actions: config.isProd ? [pagerDuty.snsCriticalAlarmTopic.arn] : [],
+          // TODO: Dead link
+          alarmDescription:
+            'Runbook: https://getpocket.atlassian.net/l/c/khqp5x57',
+        },
+        httpLatency: {
+          //Triggers non-critical alert if latency is above 500ms
+          // for 4 continuous evaluation period for 1 hour (15 mins per period)
+          evaluationPeriods: 4,
+          threshold: 500,
+          period: 900, //in seconds, 15 mins per period
+          actions: config.isProd
+            ? [pagerDuty.snsNonCriticalAlarmTopic.arn]
+            : [],
+          alarmDescription:
+            // TODO: Dead link
+            'Runbook: https://getpocket.atlassian.net/l/c/YnDN190b',
+        },
+      },
+    });
+  }
+
+  /**
+   * Create Custom log group for ECS to share across task revisions
+   * @param containerName
+   * @private
+   */
+  private createCustomLogGroup(containerName: string) {
+    const logGroup = new CloudwatchLogGroup(
+      this,
+      `${containerName}-log-group`,
+      {
+        name: `/Backend/${config.prefix}/ecs/${containerName}`,
+        retentionInDays: 90,
+        skipDestroy: true,
+        tags: config.tags,
+      },
+    );
+
+    return logGroup.name;
+  }
+}
+
+const app = new App();
+const stack = new PocketRoute(app, 'pocket-router');
+const tfEnvVersion = fs.readFileSync('.terraform-version', 'utf8');
+stack.addOverride('terraform.required_version', tfEnvVersion);
+app.synth();
