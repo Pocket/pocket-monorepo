3834b7e5dbc6a251ce25ff16eefc47b8a79c42cf
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2024-01-08 10:17:01 -0800

    feat(router): Stand up apollo router (#41)
    * feat(router): working prototype
    
    * feat(router): infrastructure and ci/cd
    
    * pass in Dockerfile path explicitly
    
    * fix region name ref
    
    * code review cleanup
    
    remove README boilerplate
    
    remove some unneeded configuration in infrastructure
    
    change health check to be publicly accessible
    
    * disable docker layer caching
    
    * use latest docker version in ci
    
    * fix casing for codedeploy app/group
    
    * fix forwarding if no jwt is present
    
    * add tracing config
    
    * fix otlp configuration
    
    * add example .env file
    
    * fix a missing $

---
+++ infrastructure/pocket-router/src/config/index.ts
@@ -0,0 +1,45 @@
+const name = 'PocketRouter';
+const domainPrefix = 'pocket-router';
+const isDev = process.env.NODE_ENV === 'development';
+const environment = isDev ? 'Dev' : 'Prod';
+const domain = isDev
+  ? `${domainPrefix}.getpocket.dev`
+  : `${domainPrefix}.getpocket.com`;
+const graphqlVariant = isDev ? 'development' : 'current';
+const isProd = process.env.NODE_ENV === 'production';
+const s3LogsBucket = isDev ? 'pocket-data-items-dev' : 'pocket-data-items';
+
+export const config = {
+  name,
+  isProd,
+  prefix: `${name}-${environment}`,
+  circleCIPrefix: `/${name}/CircleCI/${environment}`,
+  shortName: 'ROUTER',
+  environment,
+  domain,
+  s3LogsBucket,
+  isDev,
+  tags: {
+    service: name,
+    environment,
+  },
+  healthCheck: {
+    command: [
+      'CMD-SHELL',
+      'curl -f http://localhost:4000/.well-known/apollo/server-health || exit 1',
+    ],
+    interval: 15,
+    retries: 3,
+    timeout: 5,
+    startPeriod: 0,
+  },
+  envVars: {
+    graph: {
+      graphId: 'pocket-client-api',
+      graphVariant: graphqlVariant,
+    },
+  },
+  tracing: {
+    host: 'localhost',
+  },
+};
