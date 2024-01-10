77bf4046be4a5c70fae0f68477bf13ed7173bf06
Daniel Brooks <bass_rock@me.com>
2024-01-10 11:50:02 -0800

    fix(infra): adding in infra and all build steps

---
+++ infrastructure/sendgrid-data/src/main.ts
@@ -0,0 +1,77 @@
+import { Construct } from 'constructs';
+import {
+  App,
+  DataTerraformRemoteState,
+  S3Backend,
+  TerraformStack,
+} from 'cdktf';
+import { config } from './config';
+import { PocketPagerDuty, PocketVPC } from '@pocket-tools/terraform-modules';
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
+import { PagerdutyProvider } from '@cdktf/provider-pagerduty/lib/provider';
+import { ArchiveProvider } from '@cdktf/provider-archive/lib/provider';
+import { NullProvider } from '@cdktf/provider-null/lib/provider';
+import { LocalProvider } from '@cdktf/provider-local/lib/provider';
+import { ApiGateway } from './apiGateway';
+
+class SendgridData extends TerraformStack {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new AwsProvider(this, 'aws', { region: 'us-east-1' });
+    new PagerdutyProvider(this, 'pagerduty_provider', { token: undefined });
+    new ArchiveProvider(this, 'archive-provider');
+    new NullProvider(this, 'null-provider');
+    new LocalProvider(this, 'local-provider');
+
+    new S3Backend(this, {
+      bucket: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
+      key: 'sendgrid-data',
+      region: 'us-east-1',
+    });
+
+    const vpc = new PocketVPC(this, 'pocket-shared-vpc');
+    const pagerDuty = this.createPagerDuty();
+
+    new ApiGateway(this, 'apigateway-lambda', vpc, pagerDuty);
+  }
+
+  /**
+   * Create PagerDuty service for alerts
+   * @private
+   */
+  private createPagerDuty() {
+    // don't create any pagerduty resources if in dev
+    if (config.isDev) {
+      return undefined;
+    }
+
+    const incidentManagement = new DataTerraformRemoteState(
+      this,
+      'incident_management',
+      {
+        organization: 'Pocket',
+        workspaces: {
+          name: 'incident-management',
+        },
+      },
+    );
+
+    return new PocketPagerDuty(this, 'pagerduty', {
+      prefix: config.prefix,
+      service: {
+        // This is a Tier 2 service and as such only raises non-critical alarms.
+        criticalEscalationPolicyId: incidentManagement
+          .get('policy_default_non_critical_id')
+          .toString(),
+        nonCriticalEscalationPolicyId: incidentManagement
+          .get('policy_default_non_critical_id')
+          .toString(),
+      },
+    });
+  }
+}
+
+const app = new App();
+new SendgridData(app, 'fxa-webhook-proxy');
+app.synth();
