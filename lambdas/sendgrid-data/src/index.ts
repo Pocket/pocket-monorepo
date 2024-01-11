48de7b5b0344e51139f1ef86bcf7e2bc886eb2da
Daniel Brooks <bass_rock@me.com>
2024-01-10 16:59:03 -0800

    fix(sendgrid): switching to body event

--- lambdas/sendgrid-data/src/index.ts
+++ lambdas/sendgrid-data/src/index.ts
@@ -2,12 +2,37 @@ import config from './config';
 import { deliverEvents, logEventsError, logEventsReceived } from './sendgrid';
 import * as Sentry from '@sentry/serverless';
 import { captureException } from './sentry';
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 Sentry.AWSLambda.init({
   dsn: config.sentry.dsn,
   release: config.sentry.release,
   environment: config.environment,
 });
+
+/**
+ * Format the response data
+ * Source: https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html#apigateway-types-transforms
+ * @param statusCode
+ * @param message
+ * @param isError
+ */
+export function formatResponse(
+  statusCode: number,
+  message: string,
+  isError?: boolean,
+): APIGatewayProxyResult {
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      statusCode,
+      [isError ? 'error' : 'message']: message,
+    }),
+  };
+}
 
 /**
  * Note: if a handler method returns null, you need to assign a sentinel value otherwise TS doesn't like it.
@@ -27,15 +52,19 @@ Sentry.AWSLambda.init({
  * @param event
  * @param context
  */
-const eventHandler = async (event: any, context: any): Promise<boolean> => {
+const eventHandler = async (
+  event: APIGatewayEvent,
+): Promise<APIGatewayProxyResult> => {
   try {
-    logEventsReceived(event.events);
-    return await deliverEvents(event.events, event.queryParams);
+    const events = JSON.stringify(event.body);
+    logEventsReceived(events);
+    await deliverEvents(events, event.queryStringParameters);
+    return formatResponse(200, 'success', false);
   } catch (err) {
     captureException(err);
     // unless we have a requirement to return a specific error response, just throw the exception after logging
     logEventsError(event);
-    throw err;
+    return formatResponse(500, err.message, true);
   }
 };
 
