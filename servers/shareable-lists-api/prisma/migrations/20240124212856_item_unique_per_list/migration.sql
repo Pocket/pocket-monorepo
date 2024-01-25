107a959688911145170dad7fbde156061a9797f4
Kat Schelonka <kschelonka@mozilla.com>
2024-01-24 16:57:50 -0800

    Revert "feat(shareablelists): uniquenes logic in db model (#102)"
    This reverts commit 2a1f19ef759dd40a33bfaf2cec3850b3ef9e7903.

--- servers/shareable-lists-api/prisma/migrations/20240124212856_item_unique_per_list/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[listId,itemId]` on the table `ListItem` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `ListItem_listId_itemId_key` ON `ListItem`(`listId`, `itemId`);
