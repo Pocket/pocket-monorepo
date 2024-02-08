144fa6ebf79a141ab3a3a4d77126dd608b336f84
Daniel Brooks <bass_rock@me.com>
2024-02-08 07:50:48 -0800

    fix(tests): updating tests to remove chai and sinon

--- servers/v3-proxy-api/src/routes/v3Get.integration.ts
+++ servers/v3-proxy-api/src/routes/v3Get.integration.ts
@@ -1,15 +1,14 @@
 import request from 'supertest';
 import { app, server } from '../main';
 import * as Sentry from '@sentry/node';
-import sinon from 'sinon';
 import * as GraphQLCalls from '../graph/graphQLClient';
 
 describe('v3Get', () => {
   afterEach(async () => {
     server.close();
-    sinon.restore();
+    jest.restoreAllMocks();
   });
-  let expectedHeaders;
+  let expectedHeaders: { [key: string]: string };
 
   beforeAll(() => {
     expectedHeaders = {
@@ -20,15 +19,17 @@ describe('v3Get', () => {
 
   it('GET should log to Sentry and throw 5xx for unknown errors', async () => {
     //const consoleStub = sinon.stub(console, 'log');
-    const sentryStub = sinon.stub(Sentry, 'captureException');
-    sinon.stub(GraphQLCalls, 'callSavedItems').throws(new Error('test error'));
+    const sentrySpy = jest.spyOn(Sentry, 'captureException');
+    jest.spyOn(GraphQLCalls, 'callSavedItems').mockImplementation(() => {
+      throw new Error('test error');
+    });
     const response = await request(app)
       .get('/v3/get')
       .send({ consumer_key: 'test', access_token: 'test' });
     expect(response.status).toBe(500);
     //console log for some reason shows twice for the first test
     //expect(consoleStub.callCount).toBe(1);
-    expect(sentryStub.callCount).toBe(1);
+    expect(sentrySpy).toHaveBeenCalledTimes(1);
     expect(response.headers['x-error-code']).toBe(
       expectedHeaders['X-Error-Code'],
     );
@@ -36,15 +37,17 @@ describe('v3Get', () => {
   });
 
   it('POST should log to Sentry and throw 5xx for unknown errors', async () => {
-    const consoleStub = sinon.stub(console, 'log');
-    const sentryStub = sinon.stub(Sentry, 'captureException');
-    sinon.stub(GraphQLCalls, 'callSavedItems').throws(new Error('test error'));
+    const consoleSpy = jest.spyOn(console, 'log');
+    const sentrySpy = jest.spyOn(Sentry, 'captureException');
+    jest.spyOn(GraphQLCalls, 'callSavedItems').mockImplementation(() => {
+      throw new Error('test error');
+    });
     const response = await request(app)
       .post('/v3/get')
       .send({ consumer_key: 'test', access_token: 'test' });
     expect(response.status).toBe(500);
-    expect(consoleStub.callCount).toBe(1);
-    expect(sentryStub.callCount).toBe(1);
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    expect(sentrySpy).toHaveBeenCalledTimes(1);
     expect(response.headers['x-error-code']).toBe(
       expectedHeaders['X-Error-Code'],
     );
