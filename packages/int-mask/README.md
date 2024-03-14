2246e69901ccb3729f4f61daec228ca5ac59e5ce
Daniel Brooks <bass_rock@me.com>
2024-03-13 17:10:21 -0700

    feat(int-mask): moving int-mask to its own package

---
+++ packages/int-mask/README.md
@@ -0,0 +1,3 @@
+# Tracing
+
+We use this package to implement tracing within our servers
--- packages/int-mask/src/mask.ts
+++ packages/int-mask/src/mask.ts
@@ -1,27 +1,30 @@
 import _ from 'lodash';
 import crypto from 'crypto';
-import config from '../config';
 
 //mimicing web repo behavior:
 //https://github.com/Pocket/Web/blob/881d5b51646b3ccd0d7f7c7206510f263d9fef03/shared/classes/IntMask.php
 // LETTER_INDEX is used in combination with MD5_RANDOMIZER to map the few
 // letters returned in an md5, [a-f], into a larger set of characters to
 // contribute the the obfuscation, and is less clearly a md5.
-const LETTER_INDEX = new Map<string, number>(config.secrets.letterIndex);
-
-const MD5_RANDOMIZER = new Map<string, string[]>(config.secrets.md5Randomizer);
+type LetterRandomizerOptions = {
+  letterIndex: Map<string, number>;
+  md5Randomizer: Map<string, string[]>;
+};
 
 // Mix up chars from the md5 hash such that it is not so obviously an md5.
 class LetterRandomizer {
   private lastChar: string;
+  private options: LetterRandomizerOptions;
 
-  constructor() {
+  constructor(options: LetterRandomizerOptions) {
     this.lastChar = '';
+    this.options = options;
   }
 
   get(char) {
-    const lastCharModifier = LETTER_INDEX.get(this.lastChar);
-    const result = MD5_RANDOMIZER.get(char)[lastCharModifier] || char;
+    const lastCharModifier = this.options.letterIndex.get(this.lastChar);
+    const result =
+      this.options.md5Randomizer.get(char)[lastCharModifier] || char;
 
     this.lastChar = char;
 
@@ -29,22 +32,32 @@ class LetterRandomizer {
   }
 }
 
+type MaskOptions = {
+  salt1: string;
+  salt2: string;
+  letterRandomizerOptions: LetterRandomizerOptions;
+};
+
 // Create a 64 character string which can be used to hide values within.
 // Maps to PHP: IntMask#randomStringHash
 export default class Mask {
   private seed: any;
+  private options: MaskOptions;
 
-  constructor(seed) {
+  constructor(seed, options: MaskOptions) {
     this.seed = seed;
+    this.options = options;
   }
 
-  static create(seed) {
-    return new this(seed).create();
+  static create(seed, options: MaskOptions) {
+    return new this(seed, options).create();
   }
 
   create() {
     const root = this.getMd5Root();
-    const randomizer = new LetterRandomizer();
+    const randomizer = new LetterRandomizer(
+      this.options.letterRandomizerOptions,
+    );
 
     return _.chain(root)
       .toArray()
@@ -55,8 +68,8 @@ export default class Mask {
 
   getMd5Root() {
     return [
-      this.getMd5(config.secrets.salt1),
-      this.getMd5(config.secrets.salt2),
+      this.getMd5(this.options.salt1),
+      this.getMd5(this.options.salt2),
     ].join('');
   }
 
