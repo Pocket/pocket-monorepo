f89cc60a64138d531f19729cb0a63b4cd4e146ba
Daniel Brooks <bass_rock@me.com>
2024-03-28 09:38:56 -0700

    fix(secrets): adding in parameter store support

--- packages/lambda-secrets/README.md
+++ packages/lambda-secrets/README.md
@@ -1,8 +1,8 @@
 # Lambda Secrets
 
-This package is used to obtain a secret from the [Lambda Layer cache](https://aws.amazon.com/blogs/compute/using-the-aws-parameter-and-secrets-lambda-extension-to-cache-parameters-and-secrets/) when enabled.
+This package is used to obtain a secret or parameter from the [Lambda Layer cache](https://aws.amazon.com/blogs/compute/using-the-aws-parameter-and-secrets-lambda-extension-to-cache-parameters-and-secrets/) when enabled.
 
-## Usage
+## Secrets Manaager Usage
 
 ```typescript
 import {fetchSecret} from '@pocket-tools/lambda-secrets';
@@ -11,3 +11,11 @@ console.log(secret.superSecretValue);
 ```
 
 This also assumes that the secret stored in Secrets Manager is a JSON based value, which it almost always is.
+
+## Parameter Store Usage
+
+```typescript
+import {fetchParameter} from '@pocket-tools/lambda-secrets';
+const parameter = fetchParameter('/Lambda/Super/SecretParameter');
+console.log(parameter);
+```
--- packages/lambda-secrets/src/secrets.ts
+++ packages/lambda-secrets/src/secrets.ts
@@ -8,25 +8,44 @@ import fetch from 'node-fetch';
 export const fetchSecret = async (
   secretName: string,
 ): Promise<Record<string, string>> => {
+  const secretExtensionEndpoint: string = `/secretsmanager/get?secretId=${encodeURIComponent(secretName)}`;
+  const secret = await fetchFromLambda(secretExtensionEndpoint);
+  return secret.SecretString;
+};
+
+/**
+ * Requests and parses a parameter from the Lambda Layer extension
+ * @param parameterName
+ * @returns the parameter
+ */
+export const fetchParameter = async (
+  parameterName: string,
+): Promise<string> => {
+  const secretExtensionEndpoint: string = `/systemsmanager/parameters/get/?name=${encodeURIComponent(parameterName)}`;
+  const secret = await fetchFromLambda(secretExtensionEndpoint);
+  return secret.Parameter.Value;
+};
+
+const fetchFromLambda = async (url: string): Promise<Record<string, any>> => {
   if (
     !process.env.AWS_SESSION_TOKEN ||
     process.env.AWS_SESSION_TOKEN == 'undefined'
   ) {
-    throw new Error(`No AWS_SESSION_TOKEN to access the secret layer`);
+    throw new Error(`No AWS_SESSION_TOKEN to access the lambda secret layer`);
   }
 
   // Port that the lambda secrets layer is running on.
   const port = process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT ?? 2773;
-  const secretExtensionEndpoint: string = `http://localhost:${port}/secretsmanager/get?secretId=${secretName}`;
 
   // Grabs a secret according to https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html from the lambda layer
-  const secret = await fetch(secretExtensionEndpoint, {
+  // https://aws.amazon.com/blogs/compute/using-the-aws-parameter-and-secrets-lambda-extension-to-cache-parameters-and-secrets/
+  const secret = await fetch(`http://localhost:${port}${url}`, {
     headers: {
       'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN,
     },
   });
   if (!secret.ok) {
-    throw new Error(`Failed fetching secret ${secretName} from lambda layer`);
+    throw new Error(`Failed fetching ${url} from lambda secret layer`);
   }
-  return (await secret.json()).SecretString;
+  return await secret.json();
 };
