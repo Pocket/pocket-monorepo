b703ab4025be567f91eac033cbbe483d9f51794b
Daniel Brooks <bass_rock@me.com>
2024-03-27 17:40:01 -0700

    fix(cache): removing the in memory cache because lambda caches

--- packages/lambda-secrets/src/secrets.ts
+++ packages/lambda-secrets/src/secrets.ts
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
+// Grabs a secret according to https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html from the lambda layer
+
 const headers = {
   'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN,
 };
@@ -7,24 +9,16 @@ const headers = {
 // Port that the lambda secrets layer is running on.
 const port = process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT ?? 2773;
 
-const inMemoryCache = {};
-
 /**
  * Requests and parses a secret from the Lambda Layer extension
- * @param secretName 
+ * @param secretName
  * @returns the secret
  */
 export const fetchSecret = async (
   secretName: string,
 ): Promise<Record<string, string>> => {
-  if (inMemoryCache[secretName]) {
-    return inMemoryCache;
-  }
-
   const secretExtensionEndpoint: string = `http://localhost:${port}/secretsmanager/get?secretId=${secretName}`;
 
   const secret = await fetch(secretExtensionEndpoint, { headers });
-  const parsedSecret = await secret.json();
-  inMemoryCache[secretName] = parsedSecret;
-  return parsedSecret;
+  return await secret.json();
 };
