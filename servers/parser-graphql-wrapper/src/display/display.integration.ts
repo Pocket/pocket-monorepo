c98419b15f8a949398ed2fdf7bb7170d64af42ee
Daniel Brooks <bass_rock@me.com>
2024-04-15 16:18:10 -0700

    chore(tests): adding in test data

---
+++ servers/parser-graphql-wrapper/src/display/display.integration.ts
@@ -0,0 +1,129 @@
+import nock, { cleanAll } from 'nock';
+import { getRedis } from '../cache';
+import { startServer } from '../server';
+import { ApolloServer } from '@apollo/server';
+import request from 'supertest';
+import { print } from 'graphql';
+import { gql } from 'graphql-tag';
+import { IContext } from '../context';
+import { Application } from 'express';
+import { DataSource } from 'typeorm';
+import { getConnection, getSharedUrlsConnection } from '../database/mysql';
+import { ItemResolver } from '../entities/ItemResolver';
+import { IntMask } from '@pocket-tools/int-mask';
+import * as ogs from 'open-graph-scraper';
+
+jest.mock('open-graph-scraper');
+
+describe('preview', () => {
+  let app: Application;
+  let server: ApolloServer<IContext>;
+  let graphQLUrl: string;
+  let connection: DataSource;
+
+  const GET_DISPLAY = gql`
+    query display($url: String!) {
+      itemByUrl(url: $url) {
+        display {
+          title
+        }
+      }
+    }
+  `;
+
+  const testUrl = 'https://test.com';
+
+  const item = {
+    itemId: 123,
+    searchHash: '123455sdf',
+    normalUrl: testUrl,
+    resolvedId: 123,
+    hasOldDupes: false,
+  };
+
+  const parserItemId = '123';
+
+  beforeAll(async () => {
+    ({ app, server, url: graphQLUrl } = await startServer(0));
+    connection = await getConnection();
+    //Delete the items
+    const entities = connection.entityMetadatas;
+    for (const entity of entities) {
+      const repository = connection.getRepository(entity.name);
+      await repository.query(`DELETE FROM ${entity.tableName}`);
+    }
+    //Create a seed item
+    const insert = connection.manager.create(ItemResolver, item);
+    await connection.manager.save([insert]);
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.spyOn(IntMask, 'decode').mockReturnValueOnce(123);
+
+    // flush the redis cache
+    getRedis().clear();
+  });
+
+  afterAll(async () => {
+    await server.stop();
+    await getRedis().disconnect();
+    cleanAll();
+    await connection.destroy();
+    await (await getSharedUrlsConnection()).destroy();
+    jest.restoreAllMocks();
+  });
+
+  it.each([
+    {
+      parserData: {},
+      openGraphData: { error: false, result: { ogTitle: 'openGraphTitle' } },
+      expected: { title: 'openGraphTitle' },
+    },
+  ])(
+    'should return item display data',
+    async ({ parserData, openGraphData, expected }) => {
+      jest.spyOn(ogs, 'default').mockImplementation(() => {
+        return Promise.resolve({
+          html: undefined,
+          response: undefined,
+          error: false,
+          result: {},
+          // override the default
+          ...openGraphData,
+        });
+      });
+
+      nock('http://example-parser.com')
+        .get('/')
+        .query({
+          url: testUrl,
+          getItem: '1',
+          output: 'regular',
+          enableItemUrlFallback: '1',
+        })
+        .reply(200, {
+          item: {
+            item_id: parserItemId,
+            given_url: testUrl,
+            normal_url: testUrl,
+            title: 'parser test',
+            authors: [],
+            images: [],
+            videos: [],
+            resolved_id: '16822',
+            // override the default
+            ...parserData,
+          },
+        });
+
+      const variables = {
+        url: testUrl,
+      };
+      const res = await request(app)
+        .post(graphQLUrl)
+        .send({ query: print(GET_DISPLAY), variables });
+      expect(res.body.data).toEqual({ itemByUrl: { display: expected } });
+    },
+  );
+});
--- servers/parser-graphql-wrapper/src/display/display.ts
+++ servers/parser-graphql-wrapper/src/display/display.ts
@@ -5,6 +5,9 @@ import config from '../config';
 import ogs from 'open-graph-scraper';
 import { serverLogger } from '@pocket-tools/ts-logger';
 
+// We skip these domains for opengraph data because the parser grabs it from their APIs which is more accurate.
+const openGraphDomainsToSkip = ['reddit.com', 'youtube.com'];
+
 export const deriveItemSummary = async (item: Item): Promise<ItemSummary> => {
   const openGraphData = await openGraphMetadata(item);
   const itemCard = {
@@ -21,22 +24,22 @@ export const deriveItemSummary = async (item: Item): Promise<ItemSummary> => {
       : null,
     url: item.givenUrl,
     item,
-    // If we have data from opengraph, let's overwrite the Parser content
-    ...openGraphData,
+    // If we have data from opengraph, let's overwrite the Parser content, unless its a domain we should be skipping
+    ...(openGraphDomainsToSkip.some((domain) => item.givenUrl.includes(domain))
+      ? {}
+      : openGraphData),
   };
   return itemCard;
 };
 
 const openGraphMetadata = async (item: Item): Promise<Partial<ItemSummary>> => {
-  // TODO: replace with pocket user agent
-  const userAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
+  const userAgent = 'PocketParser/2.0 (+https://getpocket.com/pocketparser_ua)';
 
   const openGraphData = await ogs({
     url: item.givenUrl,
     onlyGetOpenGraphInfo: false,
     fetchOptions: { headers: { 'user-agent': userAgent } },
-    timeout: 3,
+    timeout: 3, // timeout after 3 seconds
   });
 
   if (openGraphData.error) {
@@ -49,8 +52,6 @@ const openGraphMetadata = async (item: Item): Promise<Partial<ItemSummary>> => {
 
   // We return a parital object that is expanded into the main ItemSummary object populated with Item data.
   // We use undefined because that will make the root object default to Item data when not existant
-  // TODO:
-  // authors, published date, etc
   return {
     title: result.ogTitle ?? undefined,
     image: firstImage
