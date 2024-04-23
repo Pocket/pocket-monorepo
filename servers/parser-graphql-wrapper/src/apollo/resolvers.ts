042516808db4c86112b968c90a9f145a2dfbc3b9
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2024-04-23 12:12:26 -0700

    chore: remove PocketShare prior to updating (#498)
    * remove PocketShare prior to updating
    
    * lint

--- servers/parser-graphql-wrapper/src/apollo/resolvers.ts
+++ servers/parser-graphql-wrapper/src/apollo/resolvers.ts
@@ -8,7 +8,7 @@ import {
 import { SSMLModel } from '../models/SSMLModel';
 import { fallbackPage } from '../readerView';
 import { PocketDefaultScalars } from '@pocket-tools/apollo-utils';
-import { deriveItemSummary, itemSummaryFromUrl } from '../preview';
+import { deriveItemSummary } from '../preview';
 import { URLResolver } from 'graphql-scalars';
 import { Resolvers, Videoness } from '../__generated__/resolvers-types';
 import { BoolStringParam, MediaTypeParam } from '../datasources/ParserAPI';
@@ -226,11 +226,11 @@ export const resolvers: Resolvers = {
       });
     },
   },
-  PocketShare: {
-    preview: async (parent: { targetUrl: string }, _, context) => {
-      return await itemSummaryFromUrl(parent.targetUrl, context);
-    },
-  },
+  // PocketShare: {
+  //   preview: async (parent: { targetUrl: string }, _, context) => {
+  //     return await itemSummaryFromUrl(parent.targetUrl, context);
+  //   },
+  // },
   Mutation: {
     refreshItemArticle: async (
       _source,
