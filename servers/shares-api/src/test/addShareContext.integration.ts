a8edee2beb21c55aa96f3a13969c9123f844065d
Kat Schelonka <kschelonka@mozilla.com>
2024-05-08 11:08:43 -0700

    feat(shares): delete share context if empty data is passed
    Maintains the behavior where null data does nothing.
    
    [POCKET-10088]

--- servers/shares-api/src/test/addShareContext.integration.ts
+++ servers/shares-api/src/test/addShareContext.integration.ts
@@ -6,7 +6,8 @@ import { Application } from 'express';
 import { CREATE_SHARE, ADD_SHARE_CONTEXT } from './operations';
 import { dynamoClient } from '../datasources/dynamoClient';
 import { SharesDataSourceAuthenticated } from '../datasources/shares';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { DeleteCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { config } from '../config';
 
 const uuidOverride = '0000-00-00';
 const uuidMock = jest.fn().mockImplementation(() => uuidOverride);
@@ -19,6 +20,7 @@ describe('addShareContext mutation', () => {
   // Variables/data
   const headers = { applicationisnative: 'true', userId: '1', guid: 'abc' };
   const now = Math.round(Date.now() / 1000) * 1000;
+  const db = dynamoClient();
   jest.useFakeTimers({ now });
 
   beforeAll(async () => {
@@ -30,8 +32,16 @@ describe('addShareContext mutation', () => {
     jest.restoreAllMocks();
     jest.useRealTimers();
     await server.stop();
+    db.destroy();
   });
-  afterEach(async () => {});
+  afterEach(async () => {
+    await db.send(
+      new DeleteCommand({
+        TableName: config.dynamoDb.sharesTable.name,
+        Key: { shareId: uuidOverride },
+      }),
+    );
+  });
   it.each([
     {
       context: {
@@ -160,19 +170,95 @@ describe('addShareContext mutation', () => {
     };
     expect(res.body.data).toEqual(expected);
   });
-  it('Returns generic error if error is not ConditionalCheckFailedException', async () => {
-    jest
-      .spyOn(DynamoDBDocumentClient.prototype, 'send')
-      .mockImplementation(() => Promise.reject(new Error('some error')));
+  it.each([
+    {
+      context: {
+        note: '',
+      },
+      expected: {
+        note: null,
+        highlights: [{ quote: 'never gonna give' }],
+      },
+    },
+    {
+      context: {
+        highlights: { quotes: [] },
+      },
+      expected: {
+        note: 'this is a cool video!',
+        highlights: null,
+      },
+    },
+    {
+      context: {
+        note: '',
+        highlights: { quotes: [] },
+      },
+      expected: {
+        note: null,
+        highlights: null,
+      },
+    },
+  ])('clears existing context if empty values are passed', async (fixture) => {
+    const originalContext = {
+      note: 'this is a cool video!',
+      highlights: [{ quote: 'never gonna give' }],
+    };
+    const createVars = {
+      target: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      context: {
+        note: 'this is a cool video!',
+        highlights: { quotes: ['never gonna give'] },
+      },
+    };
+    await request(app)
+      .post(graphQLUrl)
+      .set(headers)
+      .send({ query: CREATE_SHARE, variables: createVars });
     const variables = {
-      slug: 'aaaaaa-aa-aaaaaaah',
-      context: { note: 'please watch' },
+      slug: uuidOverride,
+      context: fixture.context,
     };
     const res = await request(app)
       .post(graphQLUrl)
       .set(headers)
       .send({ query: ADD_SHARE_CONTEXT, variables });
-    expect(res.body.errors[0].message).toEqual('Failed to update share record');
-    expect(res.body.errors[0].extensions.code).toEqual('INTERNAL_SERVER_ERROR');
+    const expected = {
+      addShareContext: {
+        slug: uuidOverride,
+        context: {
+          ...originalContext,
+          ...fixture.expected,
+        },
+      },
+    };
+    expect(res.body.data).toEqual(expected);
+  });
+  describe('send failure', () => {
+    let sendFailMock;
+    beforeEach(() => {
+      sendFailMock = jest
+        .spyOn(DynamoDBDocumentClient.prototype, 'send')
+        .mockImplementation(() => Promise.reject(new Error('some error')));
+    });
+    afterEach(() => {
+      sendFailMock.mockRestore();
+    });
+    it('Returns generic error if error is not ConditionalCheckFailedException', async () => {
+      const variables = {
+        slug: 'aaaaaa-aa-aaaaaaah',
+        context: { note: 'please watch' },
+      };
+      const res = await request(app)
+        .post(graphQLUrl)
+        .set(headers)
+        .send({ query: ADD_SHARE_CONTEXT, variables });
+      expect(res.body.errors[0].message).toEqual(
+        'Failed to update share record',
+      );
+      expect(res.body.errors[0].extensions.code).toEqual(
+        'INTERNAL_SERVER_ERROR',
+      );
+    });
   });
 });
