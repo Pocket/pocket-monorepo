47988adcd20fb365bb47f1a5083934e0ef612d04
Daniel Brooks <bass_rock@me.com>
2024-05-13 16:20:43 -0700

    fix(lambda): updating pnpm deploy

--- servers/parser-graphql-wrapper/src/models/pocketMetadataModels/OEmbedModel.ts
+++ servers/parser-graphql-wrapper/src/models/pocketMetadataModels/OEmbedModel.ts
@@ -12,7 +12,7 @@ import { extract } from '@extractus/oembed-extractor';
 
 export class OEmbedModel implements IPocketMetadataDataSource {
   // Use oEmbed for TikTok, and others in the future
-  matcher = /^(.*\b(tiktok\.com|ted\.com|soundcloud\.com)\b).*$/;
+  matcher = /^(.*\b(tiktok\.com|ted\.com)\b).*$/;
   ttl = 2 * 60 * 60 * 24; // 2 days of ttl cache since TikTok expires their oembed urls after 2 days of generation
   source = PocketMetadataSource.Oembed;
   version = 1;
