dc56884f25b4af70e5558e9776500dbf7a570d50
steveprokopienko <104386100+steveprokopienko@users.noreply.github.com>
2024-05-28 11:44:21 -0400

    Addressing lint error

--- infrastructure/client-api/src/main.ts
+++ infrastructure/client-api/src/main.ts
@@ -29,7 +29,6 @@ import fs from 'fs';
 
 
 import { Wafv2IpSet } from '@cdktf/provider-aws/lib/wafv2-ip-set';
-import { Wafv2RegexPatternSet } from '@cdktf/provider-aws/lib/wafv2-regex-pattern-set';
 import {
   Wafv2WebAclRule,
   Wafv2WebAcl,
