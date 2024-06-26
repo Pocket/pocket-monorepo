d1b354a592a1b9c3a8d154051a6cce1f7dab8be0
Daniel Brooks <bass_rock@me.com>
2024-06-26 10:25:49 -0700

    Revert "fix(sentry): ensuring integrations are added to sentry (#615)" (#617)
    This reverts commit ce65ed1598c1ede53824734d95572c3ecfadb91b.

--- packages/sentry/src/initSentry.ts
+++ packages/sentry/src/initSentry.ts
@@ -3,10 +3,8 @@ import * as Sentry from '@sentry/node';
 
 export const initSentry = (options?: NodeOptions) => {
   Sentry.init({
+    ...options,
     includeLocalVariables: true,
     maxValueLength: 2000,
-    // https://github.com/getsentry/sentry-javascript/blob/develop/packages/node/src/sdk/index.ts#L82-L84
-    tracesSampleRate: 0,
-    ...options,
   });
 };
