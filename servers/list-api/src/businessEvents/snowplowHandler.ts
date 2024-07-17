7f6454e5e5fe29ecf0881a421722a8e293036de6
Daniel Brooks <bass_rock@me.com>
2024-07-17 08:00:54 -0700

    feat(snowplow): adding in snowplow session id to list api

--- servers/list-api/src/businessEvents/snowplowHandler.ts
+++ servers/list-api/src/businessEvents/snowplowHandler.ts
@@ -190,6 +190,7 @@ export class SnowplowHandler {
   private addRequestInfoToTracker(data: ItemEventPayload) {
     this.tracker.setLang(data.request?.language);
     this.tracker.setDomainUserId(data.request?.snowplowDomainUserId); // possibly grab from cookie else grab from context
+    this.tracker.setSessionId(data.request?.snowplowDomainSessionId);
     this.tracker.setIpAddress(data.request?.ipAddress); // get the remote address from teh x-forwarded-for header
     this.tracker.setUseragent(data.request?.userAgent);
   }
