c2e33f5a28f93645151cb003d4c0b4c5108b96f6
Daniel Brooks <bass_rock@me.com>
2024-09-25 15:41:47 -0700

    fix(tracing): sentry sampling

--- servers/parser-graphql-wrapper/src/main.ts
+++ servers/parser-graphql-wrapper/src/main.ts
@@ -6,6 +6,7 @@ import { initSentry } from '@pocket-tools/sentry';
 // Initialize sentry
 initSentry({
   ...config.sentry,
+  tracesSampleRate: 0.01,
   debug: config.sentry.environment === 'development',
 });
 import { serverLogger } from '@pocket-tools/ts-logger';
