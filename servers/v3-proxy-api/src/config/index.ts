df4ee9c183cfe7701b5b141cb0a1a7a42dd3c747
Daniel Brooks <bass_rock@me.com>
2024-09-30 16:50:38 -0700

    fix(v3): adding in sentry sampler to v3 proxy (#788)

--- servers/v3-proxy-api/src/config/index.ts
+++ servers/v3-proxy-api/src/config/index.ts
@@ -20,6 +20,20 @@ export default {
     dsn: process.env.SENTRY_DSN || '',
     release: process.env.GIT_SHA || '',
     environment: process.env.NODE_ENV || 'development',
+    samplerFlag: 'perm.backend.sentry-trace-sampler-rate',
+  },
+  unleash: {
+    clientKey: process.env.UNLEASH_KEY || 'unleash-key-fake',
+    endpoint: process.env.UNLEASH_ENDPOINT || 'http://localhost:4242/api',
+    refreshInterval: 60 * 1000, // ms
+    timeout: 2 * 1000, // ms
+    namePrefix: 'temp.backend',
+    flags: {
+      openGraphParser: {
+        name: 'temp.backend.open_graph_parser',
+        fallback: false,
+      },
+    },
   },
   graphQLProxy: process.env.GRAPHQL_PROXY || 'https://getpocket.com/graphql',
 };
