4e884335bb375ab98cf54712c394c414c348e8c6
Kat Schelonka <kschelonka@mozilla.com>
2024-09-30 10:01:55 -0700

    feat(sentry): add feature flag sampler logic
    See also: https://github.com/Pocket/pocket-ios/pull/721
    
    [POCKET-10645]

--- packages/sentry/src/initSentry.ts
+++ packages/sentry/src/initSentry.ts
@@ -1,9 +1,26 @@
-import { NodeOptions } from '@sentry/node';
+import { type NodeOptions } from '@sentry/node';
 import * as Sentry from '@sentry/node';
 
 export const initSentry = (options?: NodeOptions) => {
   Sentry.init({
     ...options,
+    tracesSampler: (context) => {
+      // Continue trace decision if there is any parentSampled information
+      if (typeof context.parentSampled === 'boolean') {
+        return context.parentSampled;
+      }
+      // Use the tracesSampler if it exists
+      if (options?.tracesSampler != null) {
+        return options.tracesSampler(context);
+        // Otherwise, return the traces sample rate if it exists, just in case
+        // (unsure about which takes precedence)
+      } else if (options?.tracesSampleRate != null) {
+        return options.tracesSampleRate;
+        // Disable by default
+      } else {
+        return 0;
+      }
+    },
     includeLocalVariables: true,
     maxValueLength: 2000,
   });
