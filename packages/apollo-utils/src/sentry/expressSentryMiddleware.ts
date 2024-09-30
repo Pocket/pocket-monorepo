cd845f77b222ee499288fac7538267c4777ac64e
Daniel Brooks <bass_rock@me.com>
2024-09-30 10:52:10 -0700

    fix(sentry): update sentry middleware initialization (#782)

--- packages/apollo-utils/src/sentry/expressSentryMiddleware.ts
+++ packages/apollo-utils/src/sentry/expressSentryMiddleware.ts
@@ -20,10 +20,11 @@ export const sentryPocketMiddleware = (
     id: (req.headers.encodedid as string) || undefined,
     // First use the cloudfront viewer address that has been passwed through, which is always the end user (direct from our CDN https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/adding-cloudfront-headers.html),
     //   We also need to split the port off because amazon passes that through as well.
+    //   Note: Express lowercases all headers so we can safely use the lowercase version of the header.
     // then the gateway ip address which may come from web repo via x-forwaded-for
     // then the origin client ip which may be added by the alb
     ip_address:
-      (req.headers['CloudFront-Viewer-Address'] as string)?.split(':')[0] ||
+      (req.headers['cloudfront-viewer-address'] as string)?.split(':')[0] ||
       (req.headers.gatewayipaddress as string) ||
       (req.headers['origin-client-ip'] as string) ||
       undefined,
