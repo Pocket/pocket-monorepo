cb42fafc73c88a1fa0724dc66210dca29b57b7df
Daniel Brooks <bass_rock@me.com>
2024-10-15 11:45:49 -0700

    feat(tracing): add tracing to account data deleter

---
+++ servers/account-data-deleter/src/main.ts
@@ -0,0 +1,26 @@
+import { config } from './config';
+import { initSentry } from '@pocket-tools/sentry';
+
+// Init sentry MUST come before any other imports for auto instrumentation to kick in (request isolation)
+initSentry({
+  ...config.sentry,
+  skipOpenTelemetrySetup: true,
+  integrations(integrations) {
+    return integrations.filter((integration) => {
+      return integration.name !== 'NodeFetch';
+    });
+  },
+  debug: config.sentry.environment === 'development',
+});
+
+import { nodeSDKBuilder } from '@pocket-tools/tracing';
+import { unleash } from './unleash';
+nodeSDKBuilder({ ...config.tracing, unleash: unleash() }).then(() => {
+  startServer(config.app.port).then(() => {
+    serverLogger.info(
+      `🚀 Public server ready at http://localhost:${config.app.port}`,
+    );
+  });
+});
+import { serverLogger } from '@pocket-tools/ts-logger';
+import { startServer } from './server';
