f0861a2c8555a3efa1479dd008514dd6714e7144
Daniel Brooks <bass_rock@me.com>
2024-10-16 13:50:41 -0700

    feat(tracing): adding tracing to shares api

---
+++ servers/shares-api/src/unleash.ts
@@ -0,0 +1,17 @@
+import { getUnleash } from '@pocket-tools/feature-flags-client';
+import { config } from './config';
+import type { Unleash } from '@pocket-tools/feature-flags-client';
+
+let _unleash: Unleash;
+
+export function unleash(): Unleash {
+  if (_unleash != null) return _unleash;
+  _unleash = getUnleash({
+    url: config.unleash.endpoint,
+    appName: config.app.serviceName,
+    customHeaders: { Authorization: config.unleash.clientKey },
+    timeout: config.unleash.timeout,
+    refreshInterval: config.unleash.refreshInterval,
+  });
+  return _unleash;
+}
