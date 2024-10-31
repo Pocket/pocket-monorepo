a83d52ae25d59a55a0418eb5b36f088d9a9342ff
Daniel Brooks <bass_rock@me.com>
2024-10-31 12:47:51 -0700

    feat(user): updating user event tests

--- servers/user-api/src/events/eventBus/eventBusHandler.spec.ts
+++ servers/user-api/src/events/eventBus/eventBusHandler.spec.ts
@@ -1,11 +1,15 @@
 import * as Sentry from '@sentry/node';
-import { EventBridgeClient } from '@aws-sdk/client-eventbridge';
-import config from '../../config';
 import { setTimeout } from 'timers/promises';
 import EventEmitter from 'events';
 import { EventType } from '../eventType';
 import { EventBusHandler } from './eventBusHandler';
-import { AccountPayload, PocketEventType } from '@pocket-tools/event-bridge';
+import {
+  AccountPayload,
+  PocketEventType,
+  PocketEventBridgeClient,
+  AccountDelete,
+  AccountEmailUpdated,
+} from '@pocket-tools/event-bridge';
 
 /**
  * Mock event payload
@@ -18,14 +22,11 @@ const userEventData = {
 
 describe('EventBusHandler', () => {
   const clientStub: jest.SpyInstance = jest
-    .spyOn(EventBridgeClient.prototype, 'send')
-    .mockImplementation(() => Promise.resolve({ FailedEntryCount: 0 }));
+    .spyOn(PocketEventBridgeClient.prototype, 'sendPocketEvent')
+    .mockImplementation(() => Promise.resolve());
   const sentryStub: jest.SpyInstance = jest
     .spyOn(Sentry, 'captureException')
     .mockImplementation(() => '');
-  const crumbStub: jest.SpyInstance = jest
-    .spyOn(Sentry, 'addBreadcrumb')
-    .mockImplementation(() => Promise.resolve());
   const emitter = new EventEmitter();
   new EventBusHandler().init(emitter);
   const now = new Date('2022-01-01 00:00:00');
@@ -98,16 +99,13 @@ describe('EventBusHandler', () => {
       expect(clientStub).toHaveBeenCalledTimes(1);
       // Check that the payload is correct; since it's JSON, we need to decode the data
       // otherwise it also does ordering check
-      const sendCommand = clientStub.mock.calls[0][0].input as any;
-      expect(sendCommand).toHaveProperty('Entries');
-      expect(sendCommand.Entries[0]).toMatchObject({
-        Source: config.aws.eventBus.eventBridge.source,
-        EventBusName: config.aws.eventBus.name,
-        DetailType: eventType,
-      });
-      expect(JSON.parse(sendCommand.Entries[0]['Detail'])).toEqual(
-        expectedEvent,
-      );
+      const sendCommand = clientStub.mock.calls[0][0] as any;
+      const pocketEvent: AccountDelete = {
+        'detail-type': PocketEventType.ACCOUNT_DELETION,
+        detail: expectedEvent,
+        source: 'user-events',
+      };
+      expect(sendCommand).toMatchObject(pocketEvent);
     });
   });
 
@@ -146,61 +144,13 @@ describe('EventBusHandler', () => {
       expect(clientStub).toHaveBeenCalledTimes(1);
       // Check that the payload is correct; since it's JSON, we need to decode the data
       // otherwise it also does ordering check
-      const sendCommand = clientStub.mock.calls[0][0].input as any;
-      expect(sendCommand).toHaveProperty('Entries');
-      expect(sendCommand.Entries[0]).toMatchObject({
-        Source: config.aws.eventBus.eventBridge.source,
-        EventBusName: config.aws.eventBus.name,
-        DetailType: eventType,
-      });
-      expect(JSON.parse(sendCommand.Entries[0]['Detail'])).toEqual(
-        expectedEvent,
-      );
+      const sendCommand = clientStub.mock.calls[0][0] as any;
+      const pocketEvent: AccountEmailUpdated = {
+        'detail-type': PocketEventType.ACCOUNT_EMAIL_UPDATED,
+        detail: expectedEvent,
+        source: 'user-events',
+      };
+      expect(sendCommand).toMatchObject(pocketEvent);
     });
-  });
-
-  it('should log error if any events fail to send', async () => {
-    clientStub.mockRestore();
-    jest
-      .spyOn(EventBridgeClient.prototype, 'send')
-      .mockImplementationOnce(() => Promise.resolve({ FailedEntryCount: 1 }));
-    emitter.emit(EventType.ACCOUNT_DELETE, {
-      user: {
-        ...userEventData,
-      },
-      apiUser: {
-        apiId: '1',
-      },
-      eventType: EventType.ACCOUNT_DELETE,
-    });
-    // Wait just a tad in case promise needs time to resolve
-    await setTimeout(100);
-    expect(sentryStub).toHaveBeenCalledTimes(1);
-    expect(sentryStub.mock.calls[0][0].message).toContain(
-      `Failed to send event 'account-deletion' to event bus`,
-    );
-  });
-  it('should log error if send call throws error', async () => {
-    clientStub.mockRestore();
-    jest
-      .spyOn(EventBridgeClient.prototype, 'send')
-      .mockImplementationOnce(() => Promise.reject(new Error('boo!')));
-    emitter.emit(EventType.ACCOUNT_DELETE, {
-      user: {
-        ...userEventData,
-      },
-      apiUser: {
-        apiId: '1',
-      },
-      eventType: EventType.ACCOUNT_DELETE,
-    });
-    // Wait just a tad in case promise needs time to resolve
-    await setTimeout(100);
-    expect(sentryStub).toHaveBeenCalledTimes(1);
-    expect(sentryStub.mock.calls[0][0].message).toContain('boo!');
-    expect(crumbStub).toHaveBeenCalledTimes(1);
-    expect(crumbStub.mock.calls[0][0].message).toContain(
-      `Failed to send event 'account-deletion' to event bus`,
-    );
   });
 });
