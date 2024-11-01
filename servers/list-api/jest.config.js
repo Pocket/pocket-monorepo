130a95f5df818c78f89ecec9ceb59700ea67d15c
Kat Schelonka <kschelonka@mozilla.com>
2024-11-01 12:09:40 -0700

    fix: jest test setup for timezone issues
    Move chunk size to an env var so we can
    more easily manipulate in tests

--- servers/list-api/jest.config.js
+++ servers/list-api/jest.config.js
@@ -1,3 +1,9 @@
+// Our cursors for pagination require the server and code run in a single timezone.
+// Sets the timezone for date objects in tests to be the same as the database timezone.
+// This is the mimic application timezone set in the Dockerfile.
+// IMPORTANT: Always keep this timezone the same as the application timezone
+process.env.TZ = 'US/Central';
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
--- servers/list-api/jest.setup.js
+++ servers/list-api/jest.setup.js
@@ -1,11 +1,6 @@
 process.env.AWS_ACCESS_KEY_ID = 'fake-id';
 process.env.AWS_SECRET_ACCESS_KEY = 'fake-key';
 process.env.AWS_DEFAULT_REGION = 'us-east-1';
-// Our cursors for pagination require the server and code run in a single timezone.
-// Sets the timezone for date objects in tests to be the same as the database timezone.
-// This is the mimic application timezone set in the Dockerfile.
-// IMPORTANT: Always keep this timezone the same as the application timezone
-process.env.TZ = 'US/Central';
 process.env.AWS_ACCESS_KEY_ID = 'fake-id';
 process.env.AWS_SECRET_ACCESS_KEY = 'fake-key';
 process.env.AWS_DEFAULT_REGION = 'us-east-1';
