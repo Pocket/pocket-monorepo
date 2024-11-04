8cf030fa5e8242d24f8562eaa11babe457f4619a
Daniel Brooks <bass_rock@me.com>
2024-11-04 15:37:31 -0800

    fix(events): adding in event error logging

--- packages/event-bridge/src/client.ts
+++ packages/event-bridge/src/client.ts
@@ -3,10 +3,12 @@ import {
   EventBridgeClientConfig,
   PutEventsCommand,
   PutEventsCommandOutput,
+  PutEventsRequestEntry,
 } from '@aws-sdk/client-eventbridge';
 import { PocketEvent } from './events';
 import { serverLogger } from '@pocket-tools/ts-logger';
 import * as Sentry from '@sentry/node';
+import { OversizedEventError } from './errors';
 
 export interface PocketEventBridgeConfig {
   aws?: EventBridgeClientConfig;
@@ -29,16 +31,17 @@ export class PocketEventBridgeClient {
    * @param eventPayload the payload to send to event bus
    */
   async sendPocketEvent(event: PocketEvent) {
+    const entry: PutEventsRequestEntry = {
+      EventBusName: this.config.eventBus.name,
+      Detail: JSON.stringify(event.detail),
+      Source: event.source,
+      DetailType: event['detail-type'],
+    };
+
     try {
+      this.validateEventSize(entry);
       const putEventCommand = new PutEventsCommand({
-        Entries: [
-          {
-            EventBusName: this.config.eventBus.name,
-            Detail: JSON.stringify(event.detail),
-            Source: event.source,
-            DetailType: event['detail-type'],
-          },
-        ],
+        Entries: [entry],
       });
       const output: PutEventsCommandOutput =
         await this.client.send(putEventCommand);
@@ -54,8 +57,47 @@ export class PocketEventBridgeClient {
       }
     } catch (error) {
       // Don't halt program, but capture the failure in Sentry and our logs
+      Sentry.addBreadcrumb({
+        data: { event },
+        message: 'Failed to send event to event bridge',
+      });
       Sentry.captureException(error);
-      serverLogger.error(error);
+      serverLogger.error(error, { event });
     }
+  }
+
+  /**
+   * https://github.com/fredericbarthelet/typebridge/blob/master/src/Bus.ts#L87
+   * Ensures that the event is not too large to send to EventBridge (256kb)
+   * Otherwise throws an OversizedEventError
+   * @param event Event to validate
+   */
+  validateEventSize(event: PutEventsRequestEntry) {
+    const eventSize = this.computeEventSize(event);
+    if (eventSize > 256000) {
+      throw new OversizedEventError(event);
+    }
+  }
+
+  /**
+   *
+   * Compute the size of an event in bytes
+   * @param event Event to compute size of
+   * @returns size of event in bytes
+   */
+  computeEventSize(event: PutEventsRequestEntry): number {
+    let size = 0;
+
+    if (event.Time) size += 14;
+    if (event.Detail) size += Buffer.byteLength(event.Detail, 'utf8');
+    if (event.DetailType) size += Buffer.byteLength(event.DetailType, 'utf8');
+    if (event.Source) size += Buffer.byteLength(event.Source, 'utf8');
+    if (event.Resources) {
+      event.Resources.forEach((resource) =>
+        Buffer.byteLength(resource, 'utf8'),
+      );
+    }
+
+    return size;
   }
 }
