71cc42e2ed9cd2cbc9607d3d24697e9dff298990
Daniel Brooks <bass_rock@me.com>
2024-11-06 10:57:51 -0800

    feat(eventBridge): updating corpus parser hydration to use event package

--- lambdas/user-list-search-corpus-parser-hydration/src/types.ts
+++ lambdas/user-list-search-corpus-parser-hydration/src/types.ts
@@ -1,18 +1,32 @@
 // Note: These are reused (copied) from user-list-search-corpus-indexing/src/types
 // since they listen to the same event
 
+import {
+  CorpusEvent,
+  CorpusItemPayload,
+  CollectionPayload,
+  CollectionEvent,
+  PocketEventType,
+  IncomingBaseEvent,
+} from '@pocket-tools/event-bridge';
+
+type BaseEventPayload = (CollectionEvent | CorpusEvent) & IncomingBaseEvent;
+
 export type EventPayload = {
   messageId: string;
-  detailType: string;
-  detail: ApprovedItemPayload | CollectionPayload;
+  detail: BaseEventPayload['detail'];
+  detailType: PocketEventType;
+};
+export type ValidatedEventPayload = Omit<EventPayload, 'detail'> & {
+  detail: ValidLanguageApprovedItemPayload | CollectionPayload;
 };
 
 export type ValidLangEventPayload = Omit<EventPayload, 'detail'> & {
-  detail: ValidLangApprovedItemPayload | CollectionPayload;
+  detail: ValidLanguageApprovedItemPayload | CollectionPayload;
 };
 
-export type ValidLangApprovedItemPayload = Omit<
-  ApprovedItemPayload,
+export type ValidLanguageApprovedItemPayload = Omit<
+  CorpusItemPayload,
   'language'
 > & {
   language: string;
@@ -73,113 +87,9 @@ export type ParserResult = {
 
 // See infrastructure/pocket-event-bridge/src/event-rules/corpus-events/eventConfig.ts
 // and infrastructure/pocket-event-bridge/src/event-rules/collection-events/eventConfig.ts
-export const validDetailTypes = [
-  'add-approved-item',
-  'update-approved-item',
-  'collection-created',
-  'collection-updated',
+export const validDetailTypes: Array<PocketEventType> = [
+  PocketEventType.CORPUS_ITEM_ADDED,
+  PocketEventType.CORPUS_ITEM_UPDATED,
+  PocketEventType.COLLECTION_CREATED,
+  PocketEventType.COLLECTION_UPDATED,
 ];
-
-type Author = { name: string; sortOrder: number };
-
-// Types below are all copied from:
-// https://github.com/Pocket/content-monorepo/blob/7342cb5468f11fc0b3ffdddf8693b6aeeb64f26e/servers/curated-corpus-api/src/events/types.ts#L95
-export type ApprovedItemPayload = {
-  eventType: string;
-  approvedItemExternalId: string;
-  url: string;
-  authors?: Author[];
-  title?: string | null;
-  excerpt?: string | null;
-  language?: string | null;
-  publisher?: string | null;
-  imageUrl?: string | null;
-  topic?: string | null;
-  createdAt?: string | null; // UTC timestamp string
-  createdBy?: string | null; // UTC timestamp string
-  updatedAt?: string | null; // UTC timestamp string
-  datePublished?: string; // UTC timestamp string
-  isSyndicated?: boolean;
-  isCollection?: boolean;
-  domainName?: string;
-  isTimeSensitive?: boolean;
-  source?: string | null;
-  grade?: string | null;
-};
-
-// servers/shared-snowplow-consumer/src/eventConsumer/collectionEvents/types.ts
-export type CollectionPayload = {
-  collection: {
-    externalId: string;
-    slug: string;
-    title: string;
-    status: string;
-    language: string;
-    authors: CollectionAuthor[];
-    stories: CollectionStory[];
-    createdAt: number; // in seconds
-    updatedAt: number; // in seconds
-
-    imageUrl?: string;
-    labels?: Label[];
-    intro?: string;
-    curationCategory?: CurationCategory;
-    excerpt?: string;
-    partnership?: CollectionPartnership;
-    publishedAt?: number; // in seconds
-    IABParentCategory?: IABParentCategory;
-    IABChildCategory?: IABChildCategory;
-  };
-};
-
-export type CollectionStoryAuthor = { name: string; sort_order: number };
-
-export type CurationCategory = {
-  collection_curation_category_id: string;
-  name: string;
-  slug: string;
-};
-
-export type CollectionPartnership = {
-  collection_partnership_id: string;
-  name: string;
-  blurb: string;
-  image_url: string;
-  type: string;
-  url: string;
-};
-
-export type CollectionAuthor = {
-  collection_author_id: string;
-  name: string;
-  active: boolean;
-  slug?: string;
-  bio?: string;
-  image_url?: string;
-};
-
-export type CollectionStory = {
-  collection_story_id: string;
-  url: string;
-  title: string;
-  excerpt: string;
-  image_url?: string;
-  publisher?: string;
-  authors: CollectionStoryAuthor[];
-  is_from_partner: boolean;
-  sort_order?: number;
-};
-
-export type IABParentCategory = {
-  collection_iab_parent_category_id: string;
-  name: string;
-  slug: string;
-};
-
-export type IABChildCategory = {
-  collection_iab_child_category_id: string;
-  name: string;
-  slug: string;
-};
-
-export type Label = { collection_label_id: string; name: string };
