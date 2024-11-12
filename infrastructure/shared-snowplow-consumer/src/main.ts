503d170af9e2293900b1f4f07f96a68be064fac4
Daniel Brooks <bass_rock@me.com>
2024-11-12 13:50:55 -0800

    fix(sharedSnowplow): updating max recieve count

--- infrastructure/shared-snowplow-consumer/src/main.ts
+++ infrastructure/shared-snowplow-consumer/src/main.ts
@@ -75,6 +75,7 @@ class SnowplowSharedConsumerStack extends TerraformStack {
         tags: config.tags,
         redrivePolicy: JSON.stringify({
           deadLetterTargetArn: sqsDLQConsumeQueue.arn,
+          maxReceiveCounts: 3,
         }),
       },
     );
