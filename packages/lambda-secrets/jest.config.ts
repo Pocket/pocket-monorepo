389696ecd5565ec071265b0a84a33d579511478d
Daniel Brooks <bass_rock@me.com>
2025-01-03 14:46:32 -0800

    feat(esm): moving lambda-secrets  to esm

---
+++ packages/lambda-secrets/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(jest|spec).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/dist/'],
+    moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  },
+  transform: {
+    ['^.+.tsx?$']: [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        isolatedModules: true,
+        useESM: true,
+      },
+    ],
+  }
+};
+
+export default config;
