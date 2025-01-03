bebe3614ca0475d668283c42f5f2684b5fb50668
Daniel Brooks <bass_rock@me.com>
2025-01-03 08:51:53 -0800

    feat(esm): moving apollo utils to esm

--- packages/apollo-utils/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(jest|spec).[jt]s?(x)'],
-  testPathIgnorePatterns: ['/dist/'],
-  setupFiles: ['./jest.setup.js'],
-  setupFilesAfterEnv: ['jest-extended/all'],
-};
