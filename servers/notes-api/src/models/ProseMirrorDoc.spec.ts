0cc3428de048d59917f197ba623add96f4800d7a
Kat Schelonka <kschelonka@mozilla.com>
2025-01-28 09:39:55 -0800

    chore: remove noteFromQuote mutations
    Clients will handle formatting (or not) quoted notes
    using the createNote mutations instead.

--- servers/notes-api/src/models/ProseMirrorDoc.spec.ts
+++ servers/notes-api/src/models/ProseMirrorDoc.spec.ts
@@ -1,13 +1,7 @@
 import basicText from '../test/documents/basicText.json';
-import {
-  docFromMarkdown,
-  ProseMirrorDoc,
-  wrapDocInBlockQuote,
-} from './ProseMirrorDoc';
+import { docFromMarkdown, ProseMirrorDoc } from './ProseMirrorDoc';
 import { schema } from 'prosemirror-markdown';
-import fromQuote from '../test/documents/fromQuote.json';
 import badMd from '../test/documents/badMd.json';
-import { UserInputError } from '@pocket-tools/apollo-utils';
 import * as fs from 'fs';
 import path from 'path';
 
@@ -28,38 +22,6 @@ describe('ProseMirrorDoc', () => {
     it('smoke test: converts a multi-paragraph input to a string', () => {
       const doc = new ProseMirrorDoc(basicText, schema);
       expect(doc.markdown).toBeString();
-    });
-  });
-  describe('quote constructor', () => {
-    it('wraps quote in blockquote and adds attribution', () => {
-      const { input, expectedSource } = fromQuote;
-      const actual = wrapDocInBlockQuote(input, { source: 'localhost:3001' });
-      expect(actual).toEqual(expectedSource);
-    });
-    it('wraps quote in blockquote without attribution', () => {
-      const { input, expectedNoSource } = fromQuote;
-      const actual = wrapDocInBlockQuote(input);
-      expect(actual).toEqual(expectedNoSource);
-    });
-    it('throws error if an invalid node is encountered', () => {
-      const bad = {
-        type: 'doc',
-        content: [
-          {
-            type: 'invalid',
-            content: [
-              {
-                type: 'text',
-                text: '',
-              },
-            ],
-          },
-        ],
-      };
-      expect(() => wrapDocInBlockQuote(bad)).toThrowWithMessage(
-        UserInputError,
-        /.*Invalid Document.*/,
-      );
     });
   });
   describe('markdown parser', () => {
