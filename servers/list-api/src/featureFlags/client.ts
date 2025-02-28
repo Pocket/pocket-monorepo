26615583b3d90b15b540956b51c9de834312ca36
Kat Schelonka <kschelonka@mozilla.com>
2025-02-27 17:03:03 -0800

    feat(export): add new mutation with feature flag control
    For disabling export for maintenance -- let the
    queue processing finish, then we can do maintenance
    when we don't want new messages added to the export queue.

--- servers/list-api/src/featureFlags/client.ts
+++ servers/list-api/src/featureFlags/client.ts
@@ -1,10 +1,11 @@
 import { Unleash } from 'unleash-client';
 import config from '../config';
 import { getUnleash, mockUnleash } from '@pocket-tools/feature-flags-client';
+import { FeatureInterface } from 'unleash-client/lib/feature';
 
 let _unleash: Unleash;
 
-export function getClient(): Unleash {
+export function getClient(localMocks?: FeatureInterface[]): Unleash {
   const unleashConfig = {
     url: config.unleash.endpoint,
     appName: config.serviceName,
@@ -18,7 +19,7 @@ export function getClient(): Unleash {
     }
     return _unleash;
   } else {
-    const { unleash } = mockUnleash([]);
+    const { unleash } = mockUnleash(localMocks ?? []);
     return unleash;
   }
 }
