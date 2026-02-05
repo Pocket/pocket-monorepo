3267fc41af9c076a2e027b8f9f7bc77864c356e0
Jonathan Petto <jpetto@users.noreply.github.com>
2026-02-05 14:36:45 -0600

    chore(infra): tear down user list search
    - pocket decommission
    - HNT-1624

--- infrastructure/user-list-search/deploy_alerts.tf
+++
@@ -1,3 +0,0 @@
-data "aws_sns_topic" "backend-deploy-topic" {
-  name = "Backend-${local.workspace.environment}-ChatBot"
-}
--- infrastructure/user-list-search/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  default_tags {
-    tags = local.tags
-  }
-}
-
-provider "archive" {}
-
-provider "null" {}
