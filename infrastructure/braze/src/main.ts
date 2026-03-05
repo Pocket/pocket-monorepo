45ecc311708d253921cd2a67a0614ccd8492ce0d
J Petto <jpetto@users.noreply.github.com>
2026-03-05 12:03:07 -0600

    chore(infra): tear down braze domains infra (#1094)
    - pocket decommission
    - HNT-1866

--- infrastructure/braze/src/main.ts
+++ infrastructure/braze/src/main.ts
@@ -2,10 +2,7 @@ import { Construct } from 'constructs';
 import { App, S3Backend, TerraformStack } from 'cdktf';
 import { provider as awsProvider } from '@cdktf/provider-aws';
 import { config } from './config/index.ts';
-import { EmailSendDomain } from './emailSendDomain.ts';
 import * as fs from 'fs';
-import { ClickTrackingDomain } from './clickTrackingDomain.ts';
-import { DataExportBucket } from './dataExport.ts';
 
 class Braze extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -21,58 +18,6 @@ class Braze extends TerraformStack {
       dynamodbTable: `mozilla-pocket-team-${config.environment.toLowerCase()}-terraform-state`,
       key: config.name,
       region: 'us-east-1',
-    });
-
-    //The domain that we send transactional email from.
-    new EmailSendDomain(this, 'transactional_email', {
-      rootDomain: config.rootDomain,
-      subdomain: config.transactionalDomainPrefix,
-      tags: config.tags,
-      cname: 'sparkpostmail.com',
-      mx: '10 smtp.sparkpostmail.com',
-      textRecords: config.transactionalTextRecords,
-      aRecords: config.transactionalARecords,
-      useRootCNAME: false,
-    });
-
-    //The domain that we send marketing emails from.
-    new EmailSendDomain(this, 'marketing_email', {
-      rootDomain: config.rootDomain,
-      subdomain: config.marketingDomainPrefix,
-      tags: config.tags,
-      cname: 'sparkpostmail.com',
-      mx: '10 smtp.sparkpostmail.com',
-      textRecords: config.marketingTextRecords,
-      aRecords: config.marketingARecords,
-      useRootCNAME: false,
-    });
-
-    //The domain that we send newsletter emails from.
-    new EmailSendDomain(this, 'newsletter_email', {
-      rootDomain: config.rootDomain,
-      subdomain: config.newsletterDomainPrefix,
-      tags: config.tags,
-      cname: 'sparkpostmail.com',
-      mx: '10 smtp.sparkpostmail.com',
-      textRecords: config.newsletterTextRecords,
-      aRecords: config.newsletterARecords,
-      useRootCNAME: false,
-    });
-
-    //The domain that we track clicks from.
-    new ClickTrackingDomain(this, 'click_tracking', {
-      domain: config.clickTrackingDomain,
-      tags: config.tags,
-      clickTrackingOrigin: 'spgo.io',
-      wellKnownStorageDomain: config.wellKnownStorageDomain,
-    });
-
-    new DataExportBucket(this, 'data_export', {
-      brazeAccountId: config.brazeAccountId,
-      brazeExternalId: config.brazeExternalId,
-      bucket: config.brazeBucketName,
-      prefix: config.prefix,
-      tags: config.tags,
     });
   }
 }
